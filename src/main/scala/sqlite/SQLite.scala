f5bc4421264c8bb629780975aa41ee1bdf9a9224
Bryan Dina <bryansdina@gmail.com>
2019-01-03 21:41:24 -0500

    complete part 1

--- src/main/scala/sqlite/SQLite.scala
+++ src/main/scala/sqlite/SQLite.scala
@@ -3,3 +3,37 @@ package sqlite
 class SQLite {
   def void () : Boolean = true
 }
+
+object SQLite {
+
+  import java.util.Scanner
+
+  def print_prompt () = printf("db > ")
+
+  def read_input ( ) : Scanner = {
+    val scanner = new Scanner ( System.in )
+
+    if ( ! scanner.hasNext() ) {
+      printf("Error reading input\n")
+      System.exit(1)
+    }
+
+    scanner
+  }
+
+  def main ( args : Array[String] ) : Unit = {
+    while ( true ) {
+      print_prompt()
+
+      val scanner = read_input()
+
+      val command = scanner.next
+
+      if ( ".exit".equals(command) ) {
+        System.exit(0)
+      } else {
+        printf("Unrecognized command '%s'.\n", command)
+      }
+    }
+  }
+}
