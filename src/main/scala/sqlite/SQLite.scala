2602ef6b4f9aee6480201f93240182f72748f471
Bryan Dina <bryansdina@gmail.com>
2019-01-21 09:44:42 -0500

    cleanup intellij warnings

--- src/main/scala/sqlite/SQLite.scala
+++ src/main/scala/sqlite/SQLite.scala
@@ -2,20 +2,18 @@ package sqlite
 
 import scala.collection.mutable.ArrayBuffer
 
-class Page ( data : ArrayBuffer[Byte] = new ArrayBuffer[Byte](Table.PAGE_BYTES) ) {
-  def getData () : ArrayBuffer[Byte] = data
-}
+class Page ( val data : ArrayBuffer[Byte] = new ArrayBuffer[Byte](Table.PAGE_BYTES) )
 
 case object Table {
-  val PAGE_BYTES      = 4096
-  val TABLE_MAX_PAGES = 100
-  val ROWS_PER_PAGE   = PAGE_BYTES / UserRow.Column.ROW_BYTES
-  val TABLE_MAX_ROWS  = ROWS_PER_PAGE * TABLE_MAX_PAGES
+  val PAGE_BYTES      : Int = 4096
+  val TABLE_MAX_PAGES : Int = 100
+  val ROWS_PER_PAGE   : Int = PAGE_BYTES / UserRow.Column.ROW_BYTES
+  val TABLE_MAX_ROWS  : Int = ROWS_PER_PAGE * TABLE_MAX_PAGES
 }
 
 case class Table ( ) {
-  var row_num = 0
-  var pages   = ArrayBuffer[sqlite.Page](new Page ())
+  var row_num : Int = 0
+  var pages   : ArrayBuffer[sqlite.Page] = ArrayBuffer[sqlite.Page](new Page ())
 
   def row_slot ( row_num : Int ) : ( Page , Int ) = {
     val page_num = row_num / Table.ROWS_PER_PAGE
@@ -27,7 +25,7 @@ case class Table ( ) {
     }
     val row_offset  = row_num % Table.ROWS_PER_PAGE
     val byte_offset = row_offset * UserRow.Column.ROW_BYTES
-    return ( page , byte_offset )
+    ( page , byte_offset )
   }
 }
 
@@ -39,15 +37,15 @@ object StatementType {
 
 case object UserRow {
   case object Column {
-    val ID_BYTES    = 4
-    val USER_BYTES  = 40
-    val EMAIL_BYTES = 40
+    val ID_BYTES    : Int = 4
+    val USER_BYTES  : Int = 40
+    val EMAIL_BYTES : Int = 40
 
-    val ID_OFFSET    = 0
-    val USER_OFFSET  = ID_OFFSET + ID_BYTES
-    val EMAIL_OFFSET = USER_OFFSET + USER_BYTES
+    val ID_OFFSET    : Int = 0
+    val USER_OFFSET  : Int = ID_OFFSET + ID_BYTES
+    val EMAIL_OFFSET : Int = USER_OFFSET + USER_BYTES
 
-    val ROW_BYTES = ID_BYTES + USER_BYTES + EMAIL_BYTES
+    val ROW_BYTES : Int = ID_BYTES + USER_BYTES + EMAIL_BYTES
   }
 
   def serialize ( row : UserRow ) : Array[Byte] = {
@@ -77,7 +75,7 @@ case class UserRow ( id : Int = 0
                    , user  : String = ""
                    , email : String = "" ) extends Row {
 
-  override def toString () : String = s"($id, $user, $email)"
+  override def toString : String = s"($id, $user, $email)"
   def print_row () : Unit = println ( toString () )
 }
 
@@ -114,24 +112,24 @@ object SQLite {
     }
   }
 
-  def prepare_statement ( statement : String ) : ( PrepareStatement.Result , Option[Statement] ) = {
-    val insert_matcher = "insert(.*)into(.*)".r
-    val select_matcher = "select(.*)from(.*)".r
+  val INSERT_MATCHER : scala.util.matching.Regex = "insert(.*)into(.*)".r
+  val SELECT_MATCHER : scala.util.matching.Regex = "select(.*)from(.*)".r
 
+  def prepare_statement ( statement : String ) : ( PrepareStatement.Result , Option[Statement] ) = {
     if ( statement.startsWith("insert") ) {
-      val row = insert_matcher.findFirstMatchIn(statement) match {
+      val row = INSERT_MATCHER.findFirstMatchIn(statement) match {
         case Some(v) => UserRow(0,v.group(1),v.group(2))
         case _ => return ( PrepareStatement.UNRECOGNIZED_STATEMENT , None )
       }
-      return ( PrepareStatement.SUCCESS , Some(Statement(StatementType.INSERT, row)) )
+      ( PrepareStatement.SUCCESS , Some(Statement(StatementType.INSERT, row)) )
     } else if ( statement.startsWith("select") ) {
-      val row = select_matcher.findFirstMatchIn(statement) match {
+      val row = SELECT_MATCHER.findFirstMatchIn(statement) match {
         case Some(v) => UserRow(0,v.group(1),v.group(2))
         case _ => return ( PrepareStatement.UNRECOGNIZED_STATEMENT , None )
       }
-      return ( PrepareStatement.SUCCESS , Some(Statement(StatementType.SELECT , row)) )
+      ( PrepareStatement.SUCCESS , Some(Statement(StatementType.SELECT , row)) )
     } else {
-      return ( PrepareStatement.SYNTAX_ERROR , None )
+      ( PrepareStatement.SYNTAX_ERROR , None )
     }
   }
 
@@ -142,31 +140,31 @@ object SQLite {
 
     val row_to_insert = statement.row
     val ( page , slot ) = table.row_slot(table.row_num)
-    page.getData().insertAll(slot,UserRow.serialize(row_to_insert))
+    page.data.insertAll(slot,UserRow.serialize(row_to_insert))
     table.row_num += 1
 
-    return ExecuteStatement.SUCCESS
+    ExecuteStatement.SUCCESS
   }
 
   def execute_select ( statement : Statement , table : Table ) : ExecuteStatement.Result = {
     for ( i <- 0 until table.row_num ) {
       val ( page , slot ) = table.row_slot(i)
-      val row = UserRow.deserialze(page.getData().slice(slot,UserRow.Column.ROW_BYTES).toArray)
+      val row = UserRow.deserialze(page.data.slice(slot,UserRow.Column.ROW_BYTES).toArray)
       println(row)
     }
-    return ExecuteStatement.SUCCESS
+    ExecuteStatement.SUCCESS
   }
 
   def execute_statement ( statement : Statement , table : Table ) : ExecuteStatement.Result = {
     statement.statement_type match {
-      case ( StatementType.INSERT ) => execute_insert(statement , table )
-      case ( StatementType.SELECT ) => execute_select(statement , table )
+      case StatementType.INSERT => execute_insert(statement , table )
+      case StatementType.SELECT => execute_select(statement , table )
     }
   }
 
   import java.util.Scanner
 
-  def print_prompt () = printf("db > ")
+  def print_prompt () : Unit = printf("db > ")
 
   def read_input ( ) : Scanner = {
     val scanner = new Scanner ( System.in )
@@ -187,24 +185,23 @@ object SQLite {
 
       val scanner = read_input()
 
-      val command = scanner.next
+      val token = scanner.next
 
-      if ( command.startsWith(".") ) {
-        do_meta_command ( command )
+      if ( token.startsWith(".") ) {
+        do_meta_command ( token )
       }
 
-      val statement = command + scanner.nextLine
+      val line = token + scanner.nextLine
 
-      prepare_statement ( statement ) match {
-        case ( PrepareStatement.SUCCESS , Some(statement) ) => {
+      prepare_statement ( line ) match {
+        case ( PrepareStatement.SUCCESS , Some(statement) ) =>
           execute_statement ( statement , table ) match {
             case ExecuteStatement.SUCCESS    => println("executed.")
             case ExecuteStatement.TABLE_FULL => println("table full!")
           }
-        }
-        case ( PrepareStatement.SYNTAX_ERROR , None ) => println(s"Unrecognized keyword at start of '$statement'.")
-        case ( PrepareStatement.UNRECOGNIZED_STATEMENT , None ) => println(s"Unrecognized statement '$statement'.")
-        case  _ => println(s"Unknown error for '$statement'.")
+        case ( PrepareStatement.SYNTAX_ERROR , None ) => println(s"Unrecognized keyword at start of '$line'.")
+        case ( PrepareStatement.UNRECOGNIZED_STATEMENT , None ) => println(s"Unrecognized statement '$line'.")
+        case  _ => println(s"Unknown error for '$line'.")
       }
     }
   }
